f7c50b101f723d3b5e74b5f98420dab59787b291
karanja03 <wambuik03@gmail.com>
2023-07-21 12:51:10 +0300

    Comlete hooks cimple fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+import { data } from "../mocks/data";
+
+
+
+function App(){
+
+    const[image, setImage]=useState([])
+    const[loaded,setLoaded]=useState(false)
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+            
+            .then((response)=> response.json ())
+            .then((data)=>{
+                
+                setImage(data.message);
+                setLoaded(true);
+            
+        })
+    },[]);
+
+    if(!loaded) return <p>Loading ......</p>
+    return(
+        <img src={data.message} alt="A Random Dog"/>
+    )
+    
+}
+export default App;
